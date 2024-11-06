2dae8abf2d4ced4c4486498648244d45fdaeb317
ryusdar <134015274+ryusdar@users.noreply.github.com>
2024-11-06 16:02:21 -0300

    asd

--- js/formulario.js
+++ js/formulario.js
@@ -71,5 +71,8 @@ formulario.addEventListener('submit', (e) => {
 		});
 	} else {
 		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+        setTimeout(() => {
+			document.getElementById('formulario__mensaje').classList.remove('formulario__mensaje-exito-activo');
+		}, 5000);
 	}
 });
